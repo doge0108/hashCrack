d789ba2deb5a2399b69c5ca26169509df63395c8
doge0108 <tuolpail@gmail.com>
2023-11-20 00:38:01 +0700

    added function that generate random letters and numbers and generate hash

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.1"
 
+libraryDependencies += "com.outr" % "hasher_2.13" % "1.2.2"
+
 lazy val root = (project in file("."))
   .settings(
     name := "funpar-project"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,27 @@
-object Main {
+import com.roundeights.hasher.Implicits._
+import scala.util.Random
 
+object Main extends App {
+  def generateRandomString(length: Int): String = {
+    val chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    Random.alphanumeric.filter(chars.contains(_)).take(length).mkString
+  }
+
+  def generateHash(input: String): String = {
+    input.md5.hex
+  }
+
+  def generateRandomHash(): (String, String) = {
+    val randomString = generateRandomString(5) // 5 first because might take too long to crack might change after testing
+    val hash = generateHash(randomString)
+    (randomString, hash)
+  }
+
+  val (randomString, hash) = generateRandomHash()
+  println(s"Generated String: $randomString")
+  println(s"Generated Hash: $hash")
+
+//  val userWord = "test"
+//  val hash = generateHash(userWord)
+//  println(s"Hash for '$userWord': $hash")
 }
