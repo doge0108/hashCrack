1a5865390696ca2ac06fd0a5f9c293866b9720cd
doge0108 <tuolpail@gmail.com>
2023-11-21 11:12:19 +0700

    added generateCombination and crackHash that crack hash of a specified word or a random string generated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,16 +12,40 @@ object Main extends App {
   }
 
   def generateRandomHash(): (String, String) = {
-    val randomString = generateRandomString(5) // 5 first because might take too long to crack might change after testing
+    val randomString = generateRandomString(5) // 5 first might take too long to crack might change after testing
     val hash = generateHash(randomString)
     (randomString, hash)
+  }
+
+  def generateCombinations(chars: Seq[Char], length: Int): Iterator[String] = {
+    if (length == 0) {
+      Iterator("")
+    } else {
+      for {
+        char <- chars.iterator
+        rest <- generateCombinations(chars, length - 1)
+      } yield char + rest
+    }
+  }
+
+  def crackHash(targetHash: String, charset: Seq[Char], length: Int): Option[String] = {
+    generateCombinations(charset, length).find(str => generateHash(str) == targetHash)
   }
 
   val (randomString, hash) = generateRandomHash()
   println(s"Generated String: $randomString")
   println(s"Generated Hash: $hash")
 
-//  val userWord = "test"
+//  val userWord = "asdas"
 //  val hash = generateHash(userWord)
 //  println(s"Hash for '$userWord': $hash")
+  val startTime = System.nanoTime()
+  val charset = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
+  //val result = crackHash(hash, charset, userWord.length) // change this to input but will use this for now
+  val result = crackHash(hash, charset, randomString.length)
+  val endTime = System.nanoTime()
+  val duration = (endTime - startTime) / 1e9d
+  println(s"Hash cracked: $result")
+  println(f"Time taken: $duration%.2f seconds")
+
 }
